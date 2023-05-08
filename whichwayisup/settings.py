3824314226900822fa7174c22c584477ad308f2f
Claire Lemonnier <claireci.lemonnier@gmail.com>
2023-05-08 12:52:16 +0100

    test vercel settings

--- whichwayisup/settings.py
+++ whichwayisup/settings.py
@@ -182,6 +182,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static') # test vercek
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
