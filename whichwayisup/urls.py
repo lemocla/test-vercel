c5cbc8f511c309703c9c12416aab6ace67c54c21
Claire Lemonnier <claireci.lemonnier@gmail.com>
2023-05-08 13:16:12 +0100

    update url settings

--- whichwayisup/urls.py
+++ whichwayisup/urls.py
@@ -32,5 +32,6 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('reviews/', include('reviews.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
+      + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
